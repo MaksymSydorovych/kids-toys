c39445f81b4a78f7ea546896e3effcc28ddcd0b0
MaksSydorovych <makssydorovych@gmail.com>
2022-05-31 18:20:29 +0200

    1

---
+++ js/setting/api.js
@@ -0,0 +1,4 @@
+export const url = "https://wp.maksy.site/wp-json/wc/store/";
+export const productsUrl = url + "products";
+export const bannerUrl =
+  "https://wp.maksy.site/wp-content/uploads/2022/04/red-car.jpg";
